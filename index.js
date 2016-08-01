c86cd53b5b7fceba58fd932487e43db000bed4ca
laurkgol <laura.koss.goldstein@gmail.com>
2016-08-01 18:16:26 -0400

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+	$(document).ready(function(){
 //wait until doc has loaded
 var submitButton = $("#submit-btn");
 
@@ -6,23 +6,23 @@ submitButton.on("click", function(event){
 	var input = $("#city-type").val(){
 	//type NYC and image of NYC wil be the background
 	if (input === "NYC" || "New York City"|| "New York"){
-	$(".container").css("background-image" url("images/nyc.jpg"));
+	$(".container").css("background-image" "url("images/nyc.jpg")");
 	}
 	//type SF and image of SF wil be the background
 	else if (input === "San Francsico"|| "SF"|| "Bay Area"){
-	$(".container").css("background-image" url("images/sf.jpg"));
+	$(".container").css("background-image" "url("images/sf.jpg")");
 	}
 	//type LA and image of LA wil be the background
 	else if (input === "Los Angeles"|| "LA"|| "LAX"){
-	(".container").css("background-image" url("images/la.jpg"));
+	(".container").css("background-image" "url("images/la.jpg")");
 	}
 	//type Austin and image of Austin wil be the background
 	else if (input === "Austin"|| "ATX"){
-	(".container").css("background-image" url("images/austin.jpg"));
+	(".container").css("background-image" "url("images/austin.jpg")");
 	}
 	//type Sydney and image of Sydney wil be the background
 	else (input==="sydney", "SYD"){
-	(".container").css("background-image" url("images/sydney.jpg"));
+	(".container").css("background-image" "url("images/sydney.jpg")");
 	}
 }
 });
